dbb1f205863158810bb0799f60d6384758217b00
jarrisonfor <yegoma@outlook.com>
2021-08-19 18:12:58 +0100

    proyecto 4 - estructura realizada

---
+++ Proyecto 4/js/jquery.js
@@ -0,0 +1,19 @@
+$(window).keydown(function(e) {
+    e.preventDefault();
+    key = (e.keyCode) ? e.keyCode : e.which;
+    $('.key.c' + key).addClass('keydown');
+});
+
+$(window).keyup(function(e) {
+    e.preventDefault();
+    key = (e.keyCode) ? e.keyCode : e.which;
+    $('.key.c' + key).removeClass('keydown');
+});
+
+/* jQuery("#btn").on('click', function() {
+    var $txt = jQuery("#screen");
+    var caretPos = $txt[0].selectionStart;
+    var textAreaTxt = $txt.val();
+    var txtToAdd = "stuff";
+    $txt.val(textAreaTxt.substring(0, caretPos) + txtToAdd + textAreaTxt.substring(caretPos) );
+}); */
