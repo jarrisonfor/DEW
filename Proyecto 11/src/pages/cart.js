52de2a01f038595172732016943c1a6c93ed5f80
jarrisonfor <yegoma@outlook.com>
2022-02-11 23:54:07 +0000

    carrito terminado

--- Proyecto 11/src/pages/cart.js
+++ Proyecto 11/src/pages/cart.js
@@ -17,43 +17,7 @@ Vue.component('cart', {
                                         <th scope="col" class="text-right">Total</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><a href="#" class="text-danger"><i class="ri-delete-bin-3-line"></i></a></td>
-                                        <td>Apple Watch</td>
-                                        <td>
-                                            <div class="form-group mb-0">
-                                                <input type="number" class="form-control cart-qty" name="cartQty1"
-                                                    id="cartQty1" value="1">
-                                            </div>
-                                        </td>
-                                        <td>$10</td>
-                                        <td class="text-right">$500</td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#" class="text-danger"><i class="ri-delete-bin-3-line"></i></a></td>
-                                        <td>Apple iPhone</td>
-                                        <td>
-                                            <div class="form-group mb-0">
-                                                <input type="number" class="form-control cart-qty" name="cartQty2"
-                                                    id="cartQty2" value="1">
-                                            </div>
-                                        </td>
-                                        <td>$20</td>
-                                        <td class="text-right">$200</td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#" class="text-danger"><i class="ri-delete-bin-3-line"></i></a></td>
-                                        <td>Apple iPad</td>
-                                        <td>
-                                            <div class="form-group mb-0">
-                                                <input type="number" class="form-control cart-qty" name="cartQty3"
-                                                    id="cartQty3" value="1">
-                                            </div>
-                                        </td>
-                                        <td>$30</td>
-                                        <td class="text-right">$300</td>
-                                    </tr>
+                                <tbody class="cart-list">
                                 </tbody>
                             </table>
                         </div>
@@ -66,24 +30,12 @@ Vue.component('cart', {
                                 <div class="order-total table-responsive ">
                                     <table class="table table-borderless text-right">
                                         <tbody>
-                                            <!-- <tr>
-                                                <td>Sub Total :</td>
-                                                <td>$1000.00</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Shipping :</td>
-                                                <td>$0.00</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Tax(18%) :</td>
-                                                <td>$180.00</td>
-                                            </tr> -->
                                             <tr>
                                                 <td class="f-w-7 font-18">
-                                                    <h4>Amount :</h4>
+                                                    <h4>Total Amount :</h4>
                                                 </td>
                                                 <td class="f-w-7 font-18">
-                                                    <h4>$1180.00</h4>
+                                                    <h4 class="totalAmount"></h4>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -93,7 +45,7 @@ Vue.component('cart', {
                         </div>
                     </div>
                     <div class="cart-footer text-right">
-                        <button class="btn btn-success my-1">Proceed to Checkout<i
+                        <button class="btn btn-success my-1 order">Proceed to Checkout<i
                                 class="ri-arrow-right-line ml-2"></i></button>
                     </div>
                 </div>
