941da157e1a8dd42f705e88baad64b08da8f0c2e
jarrisonfor <yegoma@outlook.com>
2022-02-11 23:57:44 +0000

    contact terminado

--- Proyecto 11/src/pages/contact.js
+++ Proyecto 11/src/pages/contact.js
@@ -6,15 +6,15 @@ Vue.component('contact', {
             <form @submit="checkForm">
                 <div class="form-group">
                     <label>Name:</label>
-                    <input type="text" v-model="name" class="form-control" id="name" placeholder="Enter Name" name="name" required>
+                    <input type="text" v-model="name" class="form-control" placeholder="Enter Name" name="name" required>
                 </div>
                 <div class="form-group">
                     <label>Email:</label>
-                    <input type="email" v-model="email" class="form-control" id="name" placeholder="Enter Email" name="email" required>
+                    <input type="email" v-model="email" class="form-control" placeholder="Enter Email" name="email" required>
                 </div>
                 <div class="form-group">
                     <label>Message:</label>
-                    <textarea name="issues" v-model="message" class="form-control" id="iq" placeholder="Enter your Message" required></textarea>
+                    <textarea v-model="message" class="form-control" placeholder="Enter your Message" required></textarea>
                 </div>
                 <button type="submit" class="btn btn-success">Submit</button>
             </form>
