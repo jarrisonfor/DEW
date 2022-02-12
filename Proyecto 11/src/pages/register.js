c1b1f5c2e39e22df83e69d94dd9e18764873a8e7
jarrisonfor <yegoma@outlook.com>
2022-02-12 01:59:16 +0000

    register semi-terminado

--- Proyecto 11/src/pages/register.js
+++ Proyecto 11/src/pages/register.js
@@ -4,7 +4,7 @@ Vue.component('register', {
         <h3 class="h3">Register</h3>
         <div class="loginbox bg-white pt-2">
             <i class="bi bi-person-fill"></i>
-            <form>
+            <form @submit="checkForm">
                 <div class="loginbox-textbox">
                     <input type="text" v-model="name" class="form-control" placeholder="Name">
                     <div class="invalid-feedback">
@@ -31,7 +31,7 @@ Vue.component('register', {
                     </div>
                 </div>
                 <div class="loginbox-submit">
-                    <input type="button" class="btn btn-primary btn-block" v-on:click="checkForm" value="Register">
+                    <input type="submit" class="btn btn-primary btn-block" value="Register">
                 </div>
                 <div class="loginbox-register">
                     <button class="btn btn-link text-reset shadow-none" v-on:click="() => {this.pageChanger(4)}">Login</button>
@@ -40,7 +40,7 @@ Vue.component('register', {
         </div>
     </div>
     `,
-    props: ['pageChanger'],
+    props: ['pageChanger', 'getCookie', 'setCookie'],
     data: function () {
         return {
             name: '',
@@ -50,64 +50,84 @@ Vue.component('register', {
         }
     },
     mounted: function () {
-        /* si el usuario ya esta logeado cookie (user_id), redirigir a pagina princial */
-        if (this.getCookie('user_id')) {
+        if (this.getCookie('user_id').length > 0) {
             this.pageChanger(1);
         }
     },
     methods: {
-        setCookie: function (cname, cvalue, exdays) {
-            const d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-            let expires = "expires=" + d.toUTCString();
-            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-        },
-        getCookie: function (cname) {
-            let name = cname + "=";
-            let decodedCookie = decodeURIComponent(document.cookie);
-            let ca = decodedCookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') {
-                    c = c.substring(1);
-                }
-                if (c.indexOf(name) == 0) {
-                    return c.substring(name.length, c.length);
-                }
-            }
-            return "";
-        },
         checkForm: function (e) {
-            if (this.name.match(/^[a-zA-Z0-9]{3,20}$/) && this.email.match(/^[a-zA-Z0-9]{3,20}@[a-zA-Z0-9]{3,20}.[a-zA-Z0-9]{3,20}$/) && this.password.match(/^[a-zA-Z0-9]{3,20}$/) && this.repeatPassword.match(/^[a-zA-Z0-9]{3,20}$/)) {
-                if (this.password === this.repeatPassword) {
-                    $.ajax({
-                        url: "/server/users.php",
-                        type: "POST",
-                        dataType: 'json',
-                        data: {
-                            name: this.name,
-                            email: this.email,
-                            password: this.password,
-                        },
-                        success: (data) => {
-                            /* si el usuario ya existe en el sistema, dar error, si no inicializar cookie y redirigir a pagina principal*/
-                            if (data.status == 'error') {
-                                alert(data.message);
-                            } else {
-                                this.setCookie('user_id', data.id, 1);
-                                this.pageChanger(1);
-                            }
-                        },
-                        error: (data) => {
-                            alert('Error in server');
-                        }
-                    });
-                } else {
-                    alert('Password and Repeat Password are not the same');
-                }
+            e.preventDefault();
+            let name = $('#name');
+            let email = $('#email');
+            let password = $('#password');
+            let repeatPassword = $('#repeatPassword');
+
+            if (name.val().match(/^[a-zA-Z0-9]{3,20}$/)) {
+                name.removeClass('is-invalid');
+                name.addClass('is-valid');
             } else {
-                alert('Invalid data');
+                name.removeClass('is-valid');
+                name.addClass('is-invalid');
             }
+
+            if (email.val().match(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/)) {
+                email.removeClass('is-invalid');
+                email.addClass('is-valid');
+            } else {
+                email.removeClass('is-valid');
+                email.addClass('is-invalid');
+            }
+
+            const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/;
+
+            if (password.val().match(passwordRegex)) {
+                password.removeClass('is-invalid');
+                password.addClass('is-valid');
+            } else {
+                password.removeClass('is-valid');
+                password.addClass('is-invalid');
+            }
+
+            if (password.val().match(passwordRegex) && repeatPassword.val() == password.val()) {
+                repeatPassword.removeClass('is-invalid');
+                repeatPassword.addClass('is-valid');
+            } else {
+                repeatPassword.removeClass('is-valid');
+                repeatPassword.addClass('is-invalid');
+            }
+
+            if (name.hasClass('is-invalid') || email.hasClass('is-invalid') || password.hasClass('is-invalid') || repeatPassword.hasClass('is-invalid')) {
+                return false;
+            }
+
+            $.ajax({
+                url: "/server/users.php",
+                type: "POST",
+                dataType: 'json',
+                data: {
+                    name: name.val(),
+                    email: email.val(),
+                    password: password.val(),
+                },
+                success: (data) => {
+                    if (data.status == 'error') {
+                        Toast.fire({
+                            icon: 'error',
+                            title: data.message
+                        })
+                    } else {
+                        console.log(data);
+                        this.setCookie('user_id', data.id, 1);
+                        this.pageChanger(1);
+                    }
+                },
+                error: () => {
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Server error'
+                    })
+                }
+            });
         }
     }
 })
