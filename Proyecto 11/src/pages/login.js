547b0c6154d0a9219c350a5319cab329bedbd153
jarrisonfor <yegoma@outlook.com>
2022-02-12 00:02:53 +0000

    login terminado

--- Proyecto 11/src/pages/login.js
+++ Proyecto 11/src/pages/login.js
@@ -1,23 +1,26 @@
 Vue.component('login', {
     template: `
-        <div class="login-container animated fadeInDown bootstrap snippets bootdeys">
-            <h3 class="h3">Login</h3>
-            <div class="loginbox bg-white pt-2">
-                <i class="bi bi-person-fill"></i>
+    <div class="login-container animated fadeInDown bootstrap snippets bootdeys">
+        <h3 class="h3">Login</h3>
+        <div class="loginbox bg-white pt-2">
+            <i class="bi bi-person-fill"></i>
+            <form>
                 <div class="loginbox-textbox">
-                    <input type="text" class="form-control" placeholder="Email">
+                    <input type="email" class="form-control" id="email" placeholder="Email" required>
                 </div>
                 <div class="loginbox-textbox">
-                    <input type="text" class="form-control" placeholder="Password">
+                    <input type="password" class="form-control" id="password" placeholder="Password"
+                        required>
                 </div>
                 <div class="loginbox-submit">
-                    <input type="button" class="btn btn-primary btn-block" value="Login">
+                    <input type="submit" class="btn btn-primary btn-block" value="Login">
                 </div>
                 <div class="loginbox-register">
                     <button class="btn btn-link text-reset shadow-none" v-on:click="() => {this.pageChanger(5)}">Register</button>
                 </div>
-            </div>
+            </form>
         </div>
+    </div>
     `,
     props: ['pageChanger'],
 })
